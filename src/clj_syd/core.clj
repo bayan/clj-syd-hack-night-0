392e716ed71de67a02a1aaeafe92bb71986e5382
Bayan Khalili <bayan.net@gmail.com>
2013-03-01 00:41:52 +1100

    Brute force calculation of path size with max stations

--- src/clj_syd/core.clj
+++ src/clj_syd/core.clj
@@ -1,10 +1,45 @@
 (ns clj-syd.core
   (:gen-class ))
 
+(defn mod-pow [base i n]
+  (.intValue (.modPow (BigInteger/valueOf base)
+                      (BigInteger/valueOf i)
+                      (BigInteger/valueOf n))))
+
 (defn generate-stations
-  "Generate the stations for n"
   [n]
-  (range 1 n)) ; TODO
+  (let [generator (fn [i] [(mod-pow 2 i n) (mod-pow 3 i n)])]
+    (distinct (map generator (range 0 (inc (* 2 n)))))))
+
+(defn next-points
+  [[x y] stations]
+  (filter
+   (fn [[s-x s-y]]
+     (and (>= s-x x)
+          (>= s-y y)
+          (not= [x y] [s-x s-y])))
+   stations))
+
+(defn generate-paths
+  [n]
+  (loop [paths [{:path [[0 0]]
+                 :remaining-points (conj (generate-stations n) [n n]) }]]
+    (let [paths-with-more-steps (filter (fn [path] (> (count (path :remaining-points)) 0)) paths)]
+      (if (zero? (count paths-with-more-steps))
+        paths
+        (recur
+         (mapcat
+          (fn [{:keys [path remaining-points]}]
+            (let [candidates (next-points (last path) remaining-points)]
+              (map
+               (fn [candidate]
+                 {:path (conj path candidate)
+                  :remaining-points (next-points candidate candidates)})
+               candidates)))
+          paths-with-more-steps))))))
+
+(defn longest-path-length [n]
+  (- (count (:path (first (generate-paths n)))) 2))
 
 (defn -main
   "Project E*l*r - Problem 411"
